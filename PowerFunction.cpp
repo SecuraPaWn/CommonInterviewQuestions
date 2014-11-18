ad63e2c970725988af2e8225f0e67c9d458e67ab
Kartik Saxena <kartik.sxn@gmail.com>
2014-11-17 22:33:07 -0800

    power function implementation in C+

---
+++ PowerFunction.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+ 
+/* Function to calculate x raised to the power y */
+int power(int x, unsigned int y)
+{
+    if( y == 0)
+        return 1;
+    else if (y%2 == 0)
+        return power(x, y/2)*power(x, y/2);
+    else
+        return x*power(x, y/2)*power(x, y/2);
+ 
+}
+ 
+/* Program to test function power */
+int main()
+{
+    int x = 2;
+    unsigned int y = 3;
+    printf("%d", power(x, y));
+    system("PAUSE");
+    return EXIT_SUCCESS;
+}
+
